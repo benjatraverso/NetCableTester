37fe7bf0938b851ed31d5d4931ab05eac11ea606
Benjamín Traverso <benjatraverso@users.noreply.github.com>
2019-06-03 17:46:55 -0300

    Create single file for the NetWire tester
    initial commit

---
+++ NetCableTester.ino
@@ -0,0 +1,11 @@
+/*
+An end has a femal connector with all of its terminals connected together
+The other cable end connects to a basic board with a female DB9 that connects each end with a digital pin in the atmega328
+The CPU sends a pulse to a single digital out each time and a led brights if the wire i propperly connected to the end.
+*/
+void setup(){
+  
+}
+void loop(){
+  
+}
